3901b80da9d773ad0a295fd38a5725ce70e53492
dongik <sdimivy014@gmail.com>
2025-04-10 21:33:56 +0900

    add apps: initial commit of django apps

---
+++ django/home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.landing),
+    path('about/', views.about),
+    path('chat/', views.chat),
+    # path('food/', views.FoodList.as_view()),
+    
+]
---
+++ django/home/views.py
@@ -0,0 +1,52 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Foods
+
+
+# Create your views here.
+class FoodList(ListView):
+    """필요없어진 기능
+    """
+    model = Foods
+    template_name = 'home/food.html'
+    context_object_name = 'foods'
+
+    ordering = '-pk'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super(FoodList, self).get_context_data()
+        # context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Foods.objects.filter().count()
+        return context
+
+def landing(request):
+    
+    return render(
+        request,
+        'home/landing.html',
+        {
+            
+        }
+    )
+
+def about(request):
+    
+    return render(
+        request,
+        'home/about.html',
+        {
+            
+        }
+    )
+
+def chat(request):
+    
+    return render(
+        request,
+        'home/chat.html',
+        {
+            
+        }
+    )
